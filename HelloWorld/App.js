1b72fcef16e3eb47192c277fe34d3ddf6eaf8045
nilottttttt <nilotpalarya65@gmail.com>
2025-09-26 00:07:56 +0530

    inital hello world

---
+++ HelloWorld/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f712ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
